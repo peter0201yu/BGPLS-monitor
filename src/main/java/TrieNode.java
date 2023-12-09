7a75e3396f088658a89fa9ababd92ac07c6de741
Peter Yu <peter0201yu@gmail.com>
2023-12-08 23:48:53 -0500

    Enable delete and add unit tests

--- src/main/java/TrieNode.java
+++ src/main/java/TrieNode.java
@@ -1,12 +1,16 @@
 class TrieNode {
+    private final String bitString;
     private final int level;
     private final TrieNode[] children;
-    private boolean isEnd;
+    private boolean isPrefix;
+    private String prefix;
 
-    public TrieNode(int _level) {
+    public TrieNode(String _bitString, int _level) {
+        bitString = _bitString;
         level = _level;
         children = new TrieNode[2]; // 0 and 1 bits for binary representation
-        isEnd = false;
+        isPrefix = false;
+        prefix = "";
     }
 
     public boolean containsKey(int bit) {
@@ -17,15 +21,47 @@ class TrieNode {
         return children[bit];
     }
 
+    public String getBitString(){
+        return bitString;
+    }
+
     public void put(int bit, TrieNode node) {
         children[bit] = node;
     }
 
-    public void setEnd() {
-        isEnd = true;
+    // recursively remove child and sub-children
+    public void remove(int bit) {
+        if (children[bit].containsKey(0)){
+            children[bit].remove(0);
+        }
+        if (children[bit].containsKey(1)){
+            children[bit].remove(1);
+        }
+        children[bit] = null;
     }
 
-    public boolean isEnd() {
-        return isEnd;
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public boolean isPrefix() {
+        return isPrefix;
+    }
+    public void setPrefix(String _prefix, boolean _isPrefix) {
+        prefix = _prefix;
+        isPrefix = _isPrefix;
+    }
+
+    // For debugging: compute the prefix string for any node
+    public String computePrefixString(){
+        StringBuilder prefix = new StringBuilder();
+        String fullBitString = bitString + "0".repeat(32 - level);
+        for (int i = 0; i + 7 < 32; i += 8){
+            int decimalValue = Integer.parseInt(fullBitString.substring(i, i+8), 2);
+            prefix.append(Integer.toString(decimalValue)).append(".");
+        }
+        prefix.deleteCharAt(prefix.length()-1); // remove last dot
+        prefix.append("/").append(Integer.toString(level));
+        return prefix.toString();
     }
 }
