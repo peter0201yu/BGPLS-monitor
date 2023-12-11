28ea4c8ff6edcb9947bcd320e3bbd81410d5b482
Austin Chen <austin12chen@gmail.com>
2023-12-10 23:11:28 -0500

    synched merge differences

--- src/main/java/models/igp/RoutingGraph.java
+++ src/main/java/models/igp/RoutingGraph.java
@@ -7,16 +7,20 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class RoutingGraph {
-    public Set<String> nodes;
-    public Map<String, Set<String>> edges;
+    public Map<String, Set<String>> nodesToInterfaces; // routerIds to interfaceIds
+    public Map<String, String> interfaceToRouter; // interfaceIds to routerIds
+    public Map<String, Set<String>> edges; // routerIds to routerIds
+    public Map<String, Map<String, Float>> edgeCosts; // interfaceIds to interfaceIds to IGPMetric
 
     public RoutingGraph() {
-        this.nodes = new HashSet<>();
+        this.nodesToInterfaces = new HashMap<>();
+        this.interfaceToRouter = new HashMap<>();
         this.edges = new HashMap<>();
+        this.edgeCosts = new HashMap<>();
     }
 
     public abstract void addNode(String nodeId);
-    public abstract void addEdge(String srcId, InetAddress srcAddress, String destId, InetAddress destAddress);
+    public abstract void addEdge(String srcId, InetAddress srcAddress, String destId, InetAddress destAddress, Float metric);
     public abstract void removeNode(String nodeId);
     public abstract void removeEdge(String srcId, String destId);
 }
